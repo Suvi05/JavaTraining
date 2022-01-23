ebf45de80ffe3ace24e64b5dd2c20b2f505f5656
Suvi05 <suv143ak@gmail.com>
2022-01-23 13:07:10 +0530

    Why do we need Constructor? Three ways to initialise instance variables: 1-Using Reference Variables 2-Using Methods 3-Using Constructors

---
+++ src/JavaTraining/Constructor/UserDefinedConstructor.java
@@ -0,0 +1,11 @@
+package JavaTraining.Constructor;
+
+public class UserDefinedConstructor {
+    public static void main(String[] args) {
+        System.out.println("Main");
+        new UserDefinedConstructor();
+    }
+    UserDefinedConstructor(){ //User Defined Constructor //No Argument Constructor
+    //Good Practice
+    }
+}
