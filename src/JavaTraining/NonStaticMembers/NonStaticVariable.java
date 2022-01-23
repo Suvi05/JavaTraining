8fb11038f8aa704698bb73604f113c6407558e60
Suvi05 <suv143ak@gmail.com>
2022-01-23 13:04:39 +0530

    Non-Static Members: Non Static Block, Non Static Variable and Non Static Method

---
+++ src/JavaTraining/NonStaticMembers/NonStaticVariable.java
@@ -0,0 +1,10 @@
+package JavaTraining.NonStaticMembers;
+
+public class NonStaticVariable {
+    int num;
+    public static void main(String[] args) {
+
+        NonStaticVariable obj = new NonStaticVariable();
+        System.out.println(obj.num);
+    }
+}
