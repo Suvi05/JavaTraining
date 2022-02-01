76a51fd8921d2f3cee4d59f66e7b89dcb6ff7ee7
Suvi05 <suv143ak@gmail.com>
2022-02-01 11:40:43 +0530

    Command Line Argument

---
+++ src/JavaTraining/CommandLineArguments/CLADemo.java
@@ -0,0 +1,16 @@
+package JavaTraining.CommandLineArguments;
+
+public class CLADemo {
+    public static void main(String[] args) {
+        int len = args.length;
+
+        if(len==0){
+            System.out.println("No Inputs Provided ");
+        }else{
+            System.out.println("List of Arguments : ");
+            for(int i=0;i<len;i++){
+                System.out.println(args[i]);
+            }
+        }
+    }
+}
