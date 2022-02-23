ddacf8c3d7ec56f0d523580ec7c13485efce2a29
Suvi005 <97527579+Suvi005@users.noreply.github.com>
2022-02-23 12:04:50 +0530

    Packages           - Packages Introduction           - Importing Packages           - Sub Packages           - Java Lang Package           - Naming of Packages

--- src/JavaTraining/Packages/p1/B.java
+++ src/JavaTraining/Packages/p1/B.java
@@ -4,6 +4,6 @@ public class B {
     public static void main(String[] args) {
         A.a1();    //Static -> Class Name
         A aObject=new A();
-        aObject.a2(); //Non Static -> Object
+        aObject.a2(); //Non Static  -> Object
     }
 }
