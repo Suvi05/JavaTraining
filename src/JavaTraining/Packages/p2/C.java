99241e1a7f12f9738e18016647627d6c98cd814d
Suvi005 <97527579+Suvi005@users.noreply.github.com>
2022-02-24 08:33:31 +0530

    Comment Update in File

--- src/JavaTraining/Packages/p2/C.java
+++ src/JavaTraining/Packages/p2/C.java
@@ -1,7 +1,7 @@
 package JavaTraining.Packages.p2;
 
 import JavaTraining.Packages.p1.*; //Different Package
-                                   // To Import all the classes from p1
+                                   //* -> To Import all the classes from p1
 
 public class C {
     public static void main(String[] args) {
